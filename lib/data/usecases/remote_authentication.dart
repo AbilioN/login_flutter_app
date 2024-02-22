181275dab17a76b4030bade44e3423669eb6dd03
AbilioN <netobalby@gmail.com>
2024-02-22 14:37:19 +0100

    Authentication params now belongs to a remote resonsability

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:main_app/domain/entities/account_entity.dart';
+
 import '../../domain/usecases/usecases.dart';
 import '../http/http.dart';
 
@@ -7,6 +10,25 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
   Future<void> auth(AuthenticationParams params) async {
-    await httpClient.request(url: url, method: 'post', body: params.toJson());
+    await httpClient.request(
+        url: url,
+        method: 'post',
+        body: RemoteAuthenticationParams.fromDomain(params).toJson());
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams entity) =>
+      RemoteAuthenticationParams(
+          email: entity.email, password: entity.password);
+
+  Map toJson() => {'email': email, 'password': password};
 }
