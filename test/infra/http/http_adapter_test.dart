53068d9f025cdb972c0f7e8222c82007525af607
AbilioN <netobalby@gmail.com>
2024-02-27 20:03:16 +0100

    esure httpAdapter returns data if post returns 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,20 +1,22 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:faker/faker.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
+import 'package:main_app/data/http/http.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'http_adapter_test.mocks.dart';
 
-class HttpAdapter {
+class HttpAdapter implements MyHttpClient {
   final http.Client client;
 
   HttpAdapter(this.client);
 
-  Future<void> request({
+  Future<Map> request({
     required String url,
     required String method,
     Map? body,
@@ -25,7 +27,8 @@ class HttpAdapter {
     };
     final uri = Uri.parse(url);
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(uri, headers: headers, body: jsonBody);
+    final response = await client.post(uri, headers: headers, body: jsonBody);
+    return jsonDecode(response.body);
   }
 }
 
@@ -52,7 +55,7 @@ void main() {
         testUrl,
         headers: anyNamed('headers'),
         body: anyNamed('body'),
-      )).thenAnswer((_) async => http.Response('...', 200));
+      )).thenAnswer((_) async => http.Response('{"any_key":"any_value"}', 200));
 
       await sut.request(
         url: url,
@@ -65,7 +68,8 @@ void main() {
           'content-type': 'application/json',
           'accept': 'application/json',
         },
-        body: jsonEncode(testBody),
+        // body: jsonEncode(testBody),
+        body: '{"any_key":"any_value"}',
       )).called(1);
 
       // verify(client.post(testUrl)).called(matcher)
@@ -77,7 +81,7 @@ void main() {
         testUrl,
         headers: anyNamed('headers'),
         // body: anyNamed('body'),
-      )).thenAnswer((_) async => http.Response('...', 200));
+      )).thenAnswer((_) async => http.Response('{"any_key":"any_value"}', 200));
 
       await sut.request(
         url: url,
@@ -92,6 +96,25 @@ void main() {
         },
         // body: jsonEncode(testBody),
       )).called(1);
+
+      // verify(client.post(testUrl)).called(matcher)
+    });
+
+    test('Should return data if post returns 200', () async {
+      final testUrl = Uri.parse(url);
+      when(client.post(
+        testUrl,
+        headers: anyNamed('headers'),
+        // body: anyNamed('body'),
+      )).thenAnswer((_) async => http.Response('{"any_key":"any_value"}', 200));
+
+      final response = await sut.request(
+        url: url,
+        method: 'post',
+        // body: testBody,
+      );
+
+      expect(response, {'any_key': 'any_value'});
 
       // verify(client.post(testUrl)).called(matcher)
     });
