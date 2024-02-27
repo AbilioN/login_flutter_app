6dae0f9db5ea77d310f6fbe3f42a632bd1700896
AbilioN <netobalby@gmail.com>
2024-02-27 12:18:31 +0100

    fix testing

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,11 +1,14 @@
-import 'package:http/http.dart';
+import 'package:faker/faker.dart';
+import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'http_adapter_test.mocks.dart';
+
 class HttpAdapter {
-  final Client client;
+  final http.Client client;
 
   HttpAdapter(this.client);
 
@@ -13,19 +16,32 @@ class HttpAdapter {
     required String url,
     required String method,
   }) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
     final uri = Uri.parse(url);
-    await client.post(uri);
+    await client.post(uri, headers: headers);
   }
 }
 
-@GenerateMocks([Client])
+class ClientSpy extends Mock implements http.Client {}
+
+@GenerateMocks([ClientSpy])
 void main() {
   group('post', () {
     test('Should call post with correct values ', () async {
-      final client = MockClient((request) async {
-        return Response('...', 200);
-      });
+      final client = MockClientSpy();
       final sut = HttpAdapter(client);
+      final url = faker.internet.httpUrl();
+      when(client.post(any, headers: anyNamed('headers')))
+          .thenAnswer((_) async => http.Response('...', 200));
+
+      sut.request(url: url, method: 'post');
+      verify(client.post(Uri.parse(url), headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+      })).called(1);
     });
   });
 }
