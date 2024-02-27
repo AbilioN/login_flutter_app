1c406b8684352f34c46ec8a2cb29fdcb5dbff679
AbilioN <netobalby@gmail.com>
2024-02-27 19:04:16 +0100

    backing to testing only headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
@@ -15,12 +17,16 @@ class HttpAdapter {
   Future<void> request({
     required String url,
     required String method,
+    Map? body,
   }) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
     final uri = Uri.parse(url);
+    // await client.post(uri, headers: headers, body: jsonEncode(body));
+    // await client.post(uri, headers: headers, body: body);
+
     await client.post(uri, headers: headers);
   }
 }
@@ -32,6 +38,7 @@ void main() {
   late HttpAdapter sut;
   late ClientSpy client;
   late String url;
+  // late Uri url;
 
   setUp(() {
     client = MockClientSpy();
@@ -40,17 +47,28 @@ void main() {
   });
   group('post', () {
     test('Should call post with correct values ', () async {
-      // final client = MockClientSpy();
-      // final sut = HttpAdapter(client);
-      // final url = faker.internet.httpUrl();
-      when(client.post(Uri.parse(url), headers: anyNamed('headers')))
-          .thenAnswer((_) async => http.Response('...', 200));
+      final testUrl = Uri.parse(url);
+      when(client.post(
+        testUrl,
+        headers: anyNamed('headers'),
+        // body: anyNamed('body'),
+      )).thenAnswer((_) async => http.Response('...', 200));
 
-      sut.request(url: url, method: 'post');
-      verify(client.post(Uri.parse(url), headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json',
-      })).called(1);
+      await sut.request(
+        url: url,
+        method: 'post',
+        // body: {'any_key': 'any_value'},
+      );
+      verify(client.post(
+        Uri.parse(url),
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+        // body: "{'any_key': 'any_value'}",
+      ));
+
+      // verify(client.post(testUrl)).called(matcher)
     });
   });
 }
