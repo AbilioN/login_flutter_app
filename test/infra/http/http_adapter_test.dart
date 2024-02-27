fc5927aa79165e25303b89ec67e4a9dd1b95257c
AbilioN <netobalby@gmail.com>
2024-02-27 10:23:47 +0100

    added basic infra Http adapter test

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,32 @@
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({
+    required String url,
+    required String method,
+  }) async {
+    final uri = Uri.parse(url);
+    await client.post(uri);
+  }
+}
+
+@GenerateMocks([Client])
+void main() {
+  group('post', () {
+    test('Should call post with correct values ', () async {
+      final client = MockClient((request) async {
+        // Aqui você pode implementar o comportamento do seu mock
+        return Response('...', 200);
+      });
+      final sut = HttpAdapter(client);
+    });
+  });
+}
