3e72e8635d1fd964b1bb5ca0a8b8d5157c65a621
AbilioN <netobalby@gmail.com>
2024-03-02 11:35:27 +0100

    inserted basic login style and global app accets

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -6,10 +6,53 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    const primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
+    const primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
+    return MaterialApp(
       title: 'LoginApp',
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const LoginPage(),
+      theme: ThemeData(
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            primary: primaryColor,
+            secondary: primaryColor,
+            onPrimary: primaryColor,
+            onSecondary: primaryColor,
+            error: primaryColorDark,
+            onError: primaryColorDark,
+            onSecondaryContainer: primaryColorLight,
+            background: primaryColorLight,
+            onBackground: primaryColorDark,
+            surface: primaryColor,
+            onSurface: primaryColorDark),
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColorLight),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColorDark),
+          ),
+          alignLabelWithHint: true,
+        ),
+
+        buttonTheme: const ButtonThemeData(
+            colorScheme: ColorScheme.light(
+              primary: Colors.white,
+              secondary: primaryColorLight,
+            ),
+            buttonColor: primaryColor,
+            splashColor: primaryColorLight,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            textTheme: ButtonTextTheme.primary),
+        // textTheme: TextTheme(),
+      ),
     );
   }
 }
--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -9,12 +9,43 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
+              height: 240,
+              margin: const EdgeInsets.only(bottom: 32),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Theme.of(context).primaryColorLight,
+                      Theme.of(context).primaryColorDark,
+                    ]),
+                boxShadow: const [
+                  BoxShadow(
+                      offset: Offset(0, 0),
+                      spreadRadius: 0,
+                      blurRadius: 4,
+                      color: Colors.black)
+                ],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(80),
+                ),
+              ),
               padding: const EdgeInsets.all(10.0),
               child: const Image(image: AssetImage('lib/ui/assets/logo.png')),
             ),
-            Text('Login'.toUpperCase()),
+            Text(
+              'Login'.toUpperCase(),
+              textAlign: TextAlign.center,
+              // style: Theme.of(context).textTheme.displayLarge,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColorDark,
+              ),
+            ),
             Form(
                 child: Column(
               children: <Widget>[
